894673c9b19415f818387e9cd56ccb6e1a7e629a
flight3design <159425608+flight3design@users.noreply.github.com>
2025-12-10 22:49:49 +0200

    Update and rename v03_purchaseCrypto_step5.module.js to v04_purchaseCrypto_step5.module.js

--- simulations/sim_11/v04_purchaseCrypto_step5.module.js
+++ simulations/sim_11/v04_purchaseCrypto_step5.module.js
@@ -53,14 +53,14 @@ export default {
 }
 
 .purchaseCrypto_step5_container {
-  grid-row-gap: 40px;
+  grid-row-gap: 32px;
   background-color: #fff;
   border-radius: 20px;
   flex-direction: column;
   justify-content: flex-start;
   align-items: stretch;
   max-width: 562px;
-  padding: 40px 16px 40px 40px;
+  padding: 32px 16px 32px 32px;
   display: flex;
   overflow: hidden;
   box-shadow: 0 4px 20px #0000001a;
@@ -99,7 +99,7 @@ export default {
 }
 
 .purchaseCrypto_step5_main-container-3 {
-  grid-row-gap: 40px;
+  grid-row-gap: 32px;
   flex-direction: column;
   justify-content: flex-start;
   align-items: stretch;
@@ -336,7 +336,7 @@ export default {
 }
 
 .purchaseCrypto_step5_right-container {
-  grid-row-gap: 28px;
+  grid-row-gap: 20px;
   flex-direction: column;
   justify-content: flex-start;
   align-items: stretch;
